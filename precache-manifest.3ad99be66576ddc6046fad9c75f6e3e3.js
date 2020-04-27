f0cb9d43997c80f7522986bc0dad7e54be01fa52
Alexander Ma <alex.magamer@gmail.com>
2020-04-27 15:13:47 -0400

    Updates

---
+++ precache-manifest.3ad99be66576ddc6046fad9c75f6e3e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e30add06cca12cc9e312df2847742813",
+    "url": "/myportfolioreworked/index.html"
+  },
+  {
+    "revision": "a1205ee00ec2f36b286b",
+    "url": "/myportfolioreworked/static/css/main.e0c8bf49.chunk.css"
+  },
+  {
+    "revision": "8c8f5ad65ee388859ef7",
+    "url": "/myportfolioreworked/static/js/2.b9d6fb9f.chunk.js"
+  },
+  {
+    "revision": "49c45e15b22906ea61e3b3085d227f58",
+    "url": "/myportfolioreworked/static/js/2.b9d6fb9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1205ee00ec2f36b286b",
+    "url": "/myportfolioreworked/static/js/main.57856da1.chunk.js"
+  },
+  {
+    "revision": "49bb1c1cd5ca5bb8fdd1",
+    "url": "/myportfolioreworked/static/js/runtime-main.9036283f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myportfolioreworked/static/media/logo.5d5d9eef.svg"
+  }
+]);
